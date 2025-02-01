9b465a3ebd41ac99a7f99adf9b06a2041aaa5f1c
Animesh <animeshraj958@gmail.com>
2025-02-02 00:44:54 +0530

    changes in admin login page

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -5,12 +5,12 @@ function AdminLogin() {
     return (
         <div className={styles['container']}>
             <div className={styles.card}>
-                <div style={{ textAlign: 'center' }} ><img src="vite.svg" alt="Admin" /></div>
+                <div style={{ textAlign: 'center'}} ><img src="vite.svg" alt="Admin" style={{height:'80px', width:'80px'}} /></div>
                 <form className={styles.form}>
                     <label htmlFor="email">Admin Email:</label>
-                    <input type="mail" id="adminmail" name="adminmail" placeholder='Admin email' />
+                    <input type="mail" id="adminmail" name="adminmail" placeholder='Please enter your email' />
                     <label htmlFor="password">Password:</label>
-                    <input type="text" id="password" name="password" />
+                    <input type="password" id="password" name="password" placeholder='Please enter your password'/>
                 </form>
             </div>
         </div>
