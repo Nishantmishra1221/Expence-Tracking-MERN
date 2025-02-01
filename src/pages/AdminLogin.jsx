57acc24f296feae32152c05726747bbca6cc14a9
Animesh <animeshraj958@gmail.com>
2025-02-02 00:55:12 +0530

    login btn added

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import styles from '../styles/AdminLogin.module.css';
 
 function AdminLogin() {
+    const formData = new FormData(document.querySelector('form'));
+    console.log(Object.fromEntries(formData));
     return (
         <div className={styles['container']}>
             <div className={styles.card}>
-                <div style={{ textAlign: 'center'}} ><img src="vite.svg" alt="Admin" style={{height:'80px', width:'80px'}} /></div>
+                <div style={{ textAlign: 'center' }} ><img src="vite.svg" alt="Admin" style={{ height: '80px', width: '80px' }} /></div>
                 <form className={styles.form}>
                     <label htmlFor="email">Admin Email:</label>
                     <input type="mail" id="adminmail" name="adminmail" placeholder='Please enter your email' />
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" placeholder='Please enter your password'/>
+                    <input type="password" id="password" name="password" placeholder='Please enter your password' />
                 </form>
+                <div style={{ textAlign: 'center' }}><button className={styles.loginButton}>Login</button></div>
             </div>
         </div>
     )
