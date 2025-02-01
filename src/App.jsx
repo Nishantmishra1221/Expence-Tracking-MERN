23664cfdd399139f47ec5599ba68cd3846ea91af
arnabara4 <arnabjena4@gmail.com>
2025-02-02 04:38:12 +0530

    expense-page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 import Lander from "./pages/Lander.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
-
+import Expense from "./pages/Expense.jsx";
 const App = () => {
   return (
     <>
-      <Dashboard />
+      <Expense />
     </>
   );
 };
